b69183bee0fd2a12f273e106aaa5b77a632f23f1
Miller-RS <78825615+Miller-RS@users.noreply.github.com>
2022-07-17 22:08:36 -0500

    Update CienciaEstudiante.java

--- src/main/java/Solid/OCP/CienciaEstudiante.java
+++ src/main/java/Solid/OCP/CienciaEstudiante.java
@@ -1,7 +1,15 @@
 package Solid.OCP;
-/*
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CienciaEstudiante extends Estudiante{
-        ...completa
+
+    List<String> science= Arrays.asList("Ciencia de la computacion","Fisica");
+
+    public CienciaEstudiante(String name, String regNumber, double score, String department) {
+        super(name, regNumber, score);
+        if(science.contains(department))
+            this.department = department;
     }
 }
-*/
