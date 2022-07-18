e067491674164007df05228cf005e7ad38d91b4c
angelchaucadelacruz <88442098+angelchaucadelacruz@users.noreply.github.com>
2022-07-17 23:00:10 -0500

    Update Cliente.java

--- src/main/java/Solid/SRP/Cliente.java
+++ src/main/java/Solid/SRP/Cliente.java
@@ -1,29 +1,24 @@
 package Solid.SRP;
-/*
+
 public class Cliente {
     public static void main(String[] args) {
-        System.out.println("Demostracion de SRP");
+        System.out.println("Demostracion sin SRP");
 
         Empleado jessica = new Empleado("Jessica", "Abejita", 7.5);
         showEmpDetail(jessica);
 
-        System.out.println("\n*******\n");
+        System.out.println("\n----\n");
 
-        Empleado chalo = new Empleado ("Chalito", "Smart", 3.2);
-        showEmpDetail(chalo);
+        Empleado chalo  = new Empleado("Chalito", "Smart", 3.2);
+        showEmpDetail(chalo );
 
     }
 
     private static void showEmpDetail(Empleado emp) {
-
-        // Muestra detalles del empleado
+        GeneradorIDEmpleado id = new GeneradorIDEmpleado();
+        SeniorityChecker se= new SeniorityChecker();
         emp.displayEmpDetail();
-
-        //Genera el ID
-        // Completa
-
-        // Verifica el nivel laboral
-        // Completa
- //   }
-//}
-*/
+        System.out.println("El ID del empleado es: "+ id.generateEmpId(emp.firstName));
+        System.out.println("Este empleado es un" + " empleado " + se.checkSeniority(emp.experienceInYears));
+    }
+}
