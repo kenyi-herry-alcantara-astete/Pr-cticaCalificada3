f4acc19b7719504f64a5869d6d5107b8b3f2bd8c
angelchaucadelacruz <88442098+angelchaucadelacruz@users.noreply.github.com>
2022-07-17 23:03:00 -0500

    Update SeniorityChecker.java

--- src/main/java/Solid/SRP/SeniorityChecker.java
+++ src/main/java/Solid/SRP/SeniorityChecker.java
@@ -1,5 +1,9 @@
 package Solid.SRP;
 
 public class SeniorityChecker {
-    // completa
+    public String checkSeniority(double experienceInYears){
+
+        return  experienceInYears > 5 ?"senior":"junior";
+    }
 }
+
