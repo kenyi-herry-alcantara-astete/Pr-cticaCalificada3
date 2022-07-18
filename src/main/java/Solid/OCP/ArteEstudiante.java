f25bfd67f1c0304a3a10b51cc4070607004dbf86
Miller-RS <78825615+Miller-RS@users.noreply.github.com>
2022-07-17 22:04:31 -0500

    Update ArteEstudiante.java

--- src/main/java/Solid/OCP/ArteEstudiante.java
+++ src/main/java/Solid/OCP/ArteEstudiante.java
@@ -1,9 +1,16 @@
 package Solid.OCP;
-/*
-public class ArteEstudiante extends Estudiante{
-    public ArteEstudiante(String name, String regNumber, double score,String dept) {
-        ... completa
-    }
 
+import java.util.Arrays;
+import java.util.List;
+
+public class ArteEstudiante extends Estudiante{
+
+    List<String> arts= Arrays.asList("Historia","Literatura");
+    public ArteEstudiante(String name, String regNumber, double score, String department) {
+        super(name, regNumber, score);
+        if (arts.contains(department))
+            this.department = department;
+
+    }
 }
-*/
+
