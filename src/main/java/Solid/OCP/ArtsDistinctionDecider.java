06d08f7dedbe66aba90a05e6119a1ec8897167de
Miller-RS <78825615+Miller-RS@users.noreply.github.com>
2022-07-17 22:05:24 -0500

    Update ArtsDistinctionDecider.java

--- src/main/java/Solid/OCP/ArtsDistinctionDecider.java
+++ src/main/java/Solid/OCP/ArtsDistinctionDecider.java
@@ -1,8 +1,12 @@
-package Solid.OCP;
-/*
+ackage Solid.OCP;
+
 public class ArtsDistinctionDecider implements DistinctionDecider{
-  ...completa
-        }
+
+    @Override
+    public void EvaluationDistinction(Estudiante estudiante) {
+            if (estudiante.score > 70) {
+                System.out.println(estudiante.regNumber+" ha recibido una distincion en artes.");
+            }
     }
+
 }
-*/
